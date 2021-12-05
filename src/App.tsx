44ad61d7b97bef6e66922a83d74c320b416616e3
Andrija Gajic <andrijagajicbusiness@gmail.com>
2021-12-05 14:49:04 +0100

    fix: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import React, { lazy, Suspense } from 'react';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+
+// import { socket, SocketContext } from './context/socket';
+
+// const HomePage = lazy(() => import('./pages/HomePage/HomePage.page'));
+// const FileSystemPage = lazy(() => import('./pages/FileSystem/FileSystem.page'));
+
+function App(): JSX.Element {
+  const fallbackComponent = () => <div>Loading...</div>;
+
+  return (
+    <h1>Ide gas</h1>
+    // <SocketContext.Provider value={socket}>
+    //   <Router>
+    //     <Routes>
+    //       <Route path="/" element=
+    //         {
+    //           <Suspense fallback={fallbackComponent()}>
+    //             <HomePage />
+    //           </Suspense>
+    //         }
+    //       >
+    //       </Route>
+    //     </Routes>
+    //     <Routes>
+    //       <Route path="/file-system" element=
+    //         {
+    //           <Suspense fallback={fallbackComponent()}>
+    //             <FileSystemPage />
+    //           </Suspense>
+    //         }
+    //       >
+    //       </Route>
+    //     </Routes>
+    //   </Router>
+    // </SocketContext.Provider>
+  );
+}
+
+export default App;
